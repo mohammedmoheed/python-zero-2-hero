3503b6c24f475e452c6402d0c4391c0e90245bf8
mohammedmoheed <moheed9511@gmail.com>
2020-06-20 18:39:29 +0530

    validate user input and exercise based on conditional execution

---
+++ conditional execution/validate_user_input.py
@@ -0,0 +1,19 @@
+userinput = input('enter a number')
+try:
+    val = int(userinput)
+except:
+    val = -1
+if val >= 0:
+    print('you entered correct number')
+else:
+    print('this is not a number')   # this code validate user input
+                                    # if user enetr string it shows not a number instead of error
+
+
+a = 'hello'
+b = 0
+try:
+    b = int(a)
+except:
+    b = -1                  # this code print nothing
+
